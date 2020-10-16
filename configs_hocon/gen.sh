e8da13bc4164f87e1ecaa9ce7fe5ee0b239afeae
terry-xiaoyu <506895667@qq.com>
2020-10-16 16:54:27 +0800

    Change the structure of listeners.conf

---
+++ configs_hocon/gen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/node.conf config_examples/cluster.conf config_examples/log.conf config_examples/rpc.conf config_examples/broker.conf config_examples/listeners.conf config_examples/sysmon.conf config_examples/alarm.conf | grep . > ./emqx.conf
+
+sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/mqtt.conf | grep . > ./mqtt.conf
