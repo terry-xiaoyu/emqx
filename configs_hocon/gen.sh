59339e1e9793b50d04ddc5a61b2cbb3651f39602
terry-xiaoyu <506895667@qq.com>
2020-10-20 23:58:55 +0800

    demo(configs): Update the HOCON demo

--- configs_hocon/gen.sh
+++ configs_hocon/gen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/node.conf config_examples/cluster.conf config_examples/log.conf config_examples/rpc.conf config_examples/broker.conf config_examples/listeners.conf config_examples/sysmon.conf config_examples/alarm.conf | grep . > ./emqx.conf
+#sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/node.conf config_examples/cluster.conf config_examples/log.conf config_examples/rpc.conf config_examples/broker.conf config_examples/listeners.conf config_examples/sysmon.conf config_examples/alarm.conf | grep . > ./emqx.conf
+sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/emqx.conf | grep . > ./emqx.conf
 
 sed '/^[[:blank:]]*#/d;s/#.*//' config_examples/mqtt.conf | grep . > ./mqtt.conf
